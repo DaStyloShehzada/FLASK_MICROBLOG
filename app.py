fc31c5cf1ed9142fbff8c100b367ce88addef00e
Prem Pradeep Shah <52984999+DaStyloShehzada@users.noreply.github.com>
2021-01-05 07:55:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask, render_template, request
 import datetime
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
+load_dotenv()
 def create_app():
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://prem:prem123@microblog-application.6oewh.mongodb.net/test")
+    client = MongoClient(os.environ.get("MONGODB_URI"))
     app.db = client.microblog
     entries = []
 
